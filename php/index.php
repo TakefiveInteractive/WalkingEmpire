886b81f278edbd51b897b55742bbcdc8c0086558
Yifei Teng <tengyifei88@gmail.com>
2014-11-22 18:21:57 -0500

    fix syntax error caused by accidentally lowercapped letters

--- php/index.php
+++ php/index.php
@@ -6,10 +6,9 @@ use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 class Main {
 
-    private $authenticatedPaths = array(
-        "/update_location" => 1,
-        "/create_base" => 1,
-        "/fought_base" => 1
+    private $openPaths = array(
+        "/login" => 1,
+        "/check_cookie" => 1
     );
 
     private $slim;
@@ -20,13 +19,13 @@ class Main {
      * Function to scan requests for potentially unauthenticated attempts.
      */
     private function checkCredentials($path) {
-        if (isset($this->authenticatedPaths[$path])) {
-            // in our list of potentially dangerous requests
-            return (new \WalkingEmpire\Login\Verifier())->processCookie();
-        } else {
-            // good to go
+        if (isset($this->openPaths[$path])) {
+            // no restriction. good to go
             return TRUE;
-        }
+        } else {
+            // in our list of potentially dangerous requests
+            return (new \WalkingEmpire\Login\Verifier())->processcookie();
+       }
     }
 
     public function __construct() {
@@ -78,12 +77,23 @@ class Main {
             echo json_encode(new \WalkingEmpire\LocationResponse());
         });
 
-        $this->slim->post('/create_base', function() {
+        $this->slim->post('/add_base', function() {
             $baseManager = new \WalkingEmpire\BuildingManager();
             $baseManager->addBase();
         });
 
+        $this->slim->post('/lookup_base', function() {
+
+        });
+
         $this->slim->post('/fought_base', function() {
+        });
+
+        $this->slim->post('/build_structure', function() {
+        });
+
+        $this->slim->get('/server_stats', function() {
+            phpinfo();
         });
 
         $this->slim->run();
