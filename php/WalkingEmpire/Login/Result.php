5316415db61eb1f6b4e31b22a61cc45b12f920b3
Yifei Teng <tengyifei88@gmail.com>
2014-11-23 00:47:21 -0500

    return other user's locations

--- php/WalkingEmpire/Login/Result.php
+++ php/WalkingEmpire/Login/Result.php
@@ -15,5 +15,17 @@ class Result {
         $this->success = $success;
         $this->comment = $comment;
     }
+
+    /**
+     * Merge two Result classes. If one of them is unsucessful, the resulting Result will be unsucessful.
+     */
+    public static function mergeResults($obj1, $obj2) {
+        $obj_merged = (object) array_merge((array) $obj1, (array) $obj2);
+        // if one result fails, result fails.
+        if (!$obj1->success || !$obj2->success)
+            $obj_merged->success = FALSE;
+        return $obj_merged;
+    }
+
 }
 
--- php/WalkingEmpire/UserManager.php
+++ php/WalkingEmpire/UserManager.php
@@ -2,6 +2,16 @@
 
 namespace WalkingEmpire;
 
+class LocationInfo {
+    public $latitude;
+    public $longitude;
+
+    public function __construct($lat, $lon) {
+        $this->latitude = $lat;
+        $this->longitude = $lon;
+    }
+}
+
 class UserManager {
 
     private $input;
@@ -16,5 +26,21 @@ class UserManager {
         // create User instance
         $user = new User(App::getCookie());
         $user->setLocation($this->longitude, $this->latitude);
+    }
+
+    public function getOtherUserLocations() {
+        $allUsers = User::getAllUsers();
+        $thisId = App::getUserID();
+
+        $returnArr = array();
+        $size = count($allUsers);
+        for ($i = 0; $i < $size; $i++) {
+            // skip our user
+            if ($allUsers[$i]->getUsername() === $thisId) continue;
+            // add to the array to be returned
+            $location = $allUsers[$i]->getLocation();
+            $returnArr[$allUsers[$i]->getToken()] = new LocationInfo($location['latitude'], $location['longitude']);
+        }
+        return $returnArr;
     }
 }
